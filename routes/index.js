634dbf9c472cb6f27c7fb762168fe9cfbe398e47
Justin Peralta <justinperalta@Justins-MacBook-Air.local>
2022-08-08 07:27:45 -0500

    changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 
-import userRoutes from "./user-routes";
-import thoughtRoutes from "./thoughts-routes";
+const userRoutes = require("./user-routes");
+const thoughtRoutes = require("./thoughts-routes");
 
 router.use("/api/users", userRoutes);
 router.use("/api/thoughts", thoughtRoutes);
 
-export default router;
+module.exports = router;
